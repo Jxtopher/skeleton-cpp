6cbe4cba35a6580eb48a1b86f74da22296abf721
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-08-15 13:02:33 +0200

    Update args_parser.cpp

--- src/args_parser.cpp
+++ src/args_parser.cpp
@@ -4,15 +4,15 @@ Args args_parser(int argc, char** argv) {
     Args ret_args;
 
     boost::program_options::variables_map vm;
-    boost::program_options::options_description args(
+    boost::program_options::options_description parser(
         "**" + std::string(NAME_SOFTWARE) + "**" + " options");
-    args.add_options()("help,h", "help message")("version,v", "version")(
+    parser.add_options()("help,h", "help message")("version,v", "version")(
         "config,c",
         boost::program_options::value<std::string>(&ret_args.config),
         "file configuration json (default : \"\")");
     try {
         boost::program_options::store(
-            boost::program_options::parse_command_line(argc, argv, args), vm);
+            boost::program_options::parse_command_line(argc, argv, parser), vm);
         boost::program_options::notify(vm);
     } catch (const boost::program_options::error& ex) {
         throw std::runtime_error(std::string{} + __FILE__ + ":" +
@@ -26,7 +26,7 @@ Args args_parser(int argc, char** argv) {
     }
 
     if (vm.count("help")) {
-        std::cout << args << std::endl;
+        std::cout << parser << std::endl;
         exit(EXIT_SUCCESS);
     }
 
