3d4dca3068953019023058fef0add24040ca8d3f
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-08-19 21:55:24 +0200

    remove boost

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,10 +1,4 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "args_parser.h"
-
-int main(int argc, char** argv, char** envp) {
-    Args args = args_parser(argc, argv);
-    std::cout << "File configuration : " << args.config << std::endl;
-    return EXIT_SUCCESS;
-}
+int main(int argc, char** argv, char** envp) { return EXIT_SUCCESS; }
