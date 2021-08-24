40e862a3ca2861d743e6427351584276912c4b87
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-08-24 22:15:43 +0200

    Revert "remove boost"
    This reverts commit 3d4dca3068953019023058fef0add24040ca8d3f.

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,4 +1,10 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(int argc, char** argv, char** envp) { return EXIT_SUCCESS; }
+#include "args_parser.h"
+
+int main(int argc, char** argv, char** envp) {
+    Args args = args_parser(argc, argv);
+    std::cout << "File configuration : " << args.config << std::endl;
+    return EXIT_SUCCESS;
+}
